afed6a1bffae06473ca1b84a692c68c35c7f6a79
Ivan Vynogradov <Ivan Vynogradov>
2022-04-17 22:49:41 +0300

    HW1. implementation of Array storage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,10 @@
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +12,63 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(this.storage, null);
     }
 
     void save(Resume r) {
+        int firstNullIndex = IntStream.range(0, storage.length)
+                .filter(i -> storage[i] == null)
+                .findFirst()
+                .orElse(-1);
+
+        if(firstNullIndex != -1){
+            this.storage[firstNullIndex] = r;
+        }else {
+            throw new RuntimeException("Storage is full");
+        }
+
     }
 
     Resume get(String uuid) {
-        return null;
+        Optional<Resume> optional = Arrays.stream(this.storage)
+                .filter(Objects::nonNull)
+                .filter(resume -> resume.uuid.equals(uuid))
+                .findFirst();
+
+        //Not sure good solution to return new object. possible notFoundException is better solution
+        // but the task not allow to change of methods signature
+        return optional.orElseGet(Resume::new);
     }
 
     void delete(String uuid) {
+        int index = this.findIndexByUuid(uuid);
+        if(index != -1){
+            System.arraycopy(this.storage, index + 1, this.storage, index, this.storage.length - index - 1);
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.stream(this.storage).filter(Objects::nonNull).toArray(Resume[]::new);
     }
 
     int size() {
-        return 0;
+        return (int) Arrays.stream(this.storage).filter(Objects::nonNull).count();
+    }
+
+    private int findIndexByUuid(String uuid){
+        int resumeIndex = -1;
+        for (int i = 0; i < this.storage.length; i++) {
+            if(this.storage[i] == null){
+                return resumeIndex;
+            }else {
+                if(this.storage[i] != null && this.storage[i].uuid.equals(uuid)){
+                    resumeIndex = i;
+                }
+            }
+        }
+        return resumeIndex;
     }
 }
